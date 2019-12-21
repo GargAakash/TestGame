56fa6294b7f4deaa8fd1c3f4fee40f6efc998319
Aakash Garg <aakash.garg@mni.thm.de>
2019-12-21 22:33:20 +0100

    render canvas

--- UI/Scripts/startpage.js
+++ UI/Scripts/startpage.js
@@ -1,20 +1,12 @@
-"use strict";
-exports.__esModule = true;
-var CanvasAnimation = /** @class */ (function () {
-    function CanvasAnimation(canvas) {
-        var _this = this;
-        this.canvas = canvas;
-        this.context = this.canvas.getContext('2d');
-        window.requestAnimationFrame(function () { return _this.draw(); });
-    }
-    CanvasAnimation.prototype.draw = function () {
-        // do stuff
-        var _this = this;
-        window.requestAnimationFrame(function () { return _this.draw(); });
-    };
-    return CanvasAnimation;
-}());
-exports.CanvasAnimation = CanvasAnimation;
-// Below is the way to call animation
-var canvas = document.getElementById('canvas');
-new CanvasAnimation(canvas);
+var canvas;
+var ctx;
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 50, 100);
+}
+window.onload = function () {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext("2d");
+    gameLoop();
+};
--- UI/Scripts/startpage.ts
+++ UI/Scripts/startpage.ts
@@ -1,18 +1,13 @@
-export class CanvasAnimation {
-    private readonly context: CanvasRenderingContext2D;
+var canvas: HTMLCanvasElement;
+var ctx: CanvasRenderingContext2D;
 
-    constructor(private readonly canvas: HTMLCanvasElement) {
-        this.context = this.canvas.getContext('2d');
-        window.requestAnimationFrame(() => this.draw());
-    }
-
-    draw() {
-        // do stuff
-        
-        window.requestAnimationFrame(() => this.draw());
-    }
+function gameLoop(){
+    requestAnimationFrame(gameLoop);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0,0,50,100);
 }
-
-// Below is the way to call animation
-const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-new CanvasAnimation(canvas);
+window.onload = () => {
+    canvas = <HTMLCanvasElement>document.getElementById('canvas');
+    ctx = canvas.getContext("2d");
+    gameLoop();
+}
